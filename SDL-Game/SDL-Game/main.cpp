290bf839686dd9b6794bef45b982bd376f330367
KostadinovK <kostadinovk2001@gmail.com>
2018-11-25 20:49:53 +0200

    Locked the FPS to 60

--- SDL-Game/SDL-Game/main.cpp
+++ SDL-Game/SDL-Game/main.cpp
@@ -6,13 +6,29 @@ Game* game = nullptr;
 
 int main(int argc, char* argv[])
 {
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart = 0;
+	int frameTime = 0;
+
+
 	game = new Game();
 	game->init("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 	while(game->running())
 	{
+		frameStart = SDL_GetTicks();
+
 		game->handleEvent();
 		game->update();
 		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if(frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->clean();
